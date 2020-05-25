4403efa2e15fbec92a15b68391732f105011ec4c
mvardzhieva <maria9809@abv.bg>
2020-05-25 23:16:06 +0300

    Sweet messages pet-profile

---
+++ js/pet-profile.js
@@ -0,0 +1,24 @@
+document.getElementById("txt").style.visibility = 'hidden';
+document.getElementById("txt2").style.visibility = 'hidden';
+
+function Favourute() {
+    var x = document.getElementById("fav");
+    if (x.innerHTML === "Add to favourites") {
+        swal("", "The pet is in FAVOURITES!", "success");
+        x.innerHTML = "Remove from favourites";
+        document.getElementById("txt").style.visibility = 'visible';
+    } else {
+        swal("", "The pet is removed from FAVOURITES!", "success");
+        x.innerHTML = "Add to favourites";
+        document.getElementById("txt").style.visibility = 'hidden';
+    }
+}
+
+function Adoption() {
+    var x = document.getElementById("adopt");
+    if (x.innerHTML === "Request adoption") {
+        swal("", "Adoption request has been sent!", "success");
+        document.getElementById("adopt").disabled = true;
+        document.getElementById("txt2").style.visibility = 'visible';
+    }
+}
