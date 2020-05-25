5ea89fb60ff680e907f94acfe597e4553003a8b6
Deniza Topalova <deniza@gtmhub.com>
2020-05-25 11:34:11 +0300

    Made the Delete and Send Request buttons work on my profile page

--- js/myprofile.js
+++ js/myprofile.js
@@ -4,9 +4,32 @@ function approve(requestDiv) {
     let request = document.getElementById(requestDiv);
     request.remove();
 }
+
 function reject(requestDiv) {
     swal("Cancelled", "You have rejected the request!", "error");
 
     let request = document.getElementById(requestDiv);
     request.remove();
+}
+
+function deletePet(id, parentDivId) {
+    swal("The pet has been deleted from the collection", "", "info");
+
+    let petToDelete = document.getElementById(id);
+    let myPetsRow = document.getElementById(parentDivId);
+
+    myPetsRow.removeChild(petToDelete);
+}
+
+function requestAdoption(reqBtnId) {
+    
+    swal("If you want, you can write a message to the owner:", {
+        content: "input",
+      })
+      .then(() => {
+        swal("Yay!", "The request has been sent to the owner!", "success");
+      });
+
+      let reqBtn = document.getElementById(reqBtnId);
+      reqBtn.disabled = true;
 }
